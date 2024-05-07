d37f42372089cddb4419de62f10c59e7eb0c0d7b
Juwan <junhuanchen@qq.com>
2024-05-07 11:56:28 +0800

    [20240507]

--- xmake-repo/packages/s/sqlite3/xmake.lua
+++ xmake-repo/packages/s/sqlite3/xmake.lua
@@ -29,6 +29,7 @@ package("sqlite3")
     add_versions("3.39.0+200", "852be8a6183a17ba47cee0bbff7400b7aa5affd283bf3beefc34fcd088a239de")
     add_versions("3.43.0+200", "6d422b6f62c4de2ca80d61860e3a3fb693554d2f75bb1aaca743ccc4d6f609f0")
 
+
     if is_plat("macosx", "linux", "bsd") then
         add_syslinks("pthread", "dl")
     end
